cbe954fc00bfa12d68cf4388ebf812b3ebc2b5d1
smurphy2230 <smurphy2230@gmail.com>
2020-03-30 19:46:34 -0400

    using className property

--- ch9ex5.js
+++ ch9ex5.js
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chapter 9, Example 5</title>
-</head>
-<body>
-    
-</body>
-</html>
+let divAdvert = document.getElementById("divAdvert");
+divAdvert.className = "new-style";
