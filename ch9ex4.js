d608750de46fe8ab30b4a40d1dd6638dba3da180
smurphy2230 <smurphy2230@gmail.com>
2020-03-30 19:32:26 -0400

    using the style object

--- ch9ex4.js
+++ ch9ex4.js
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chapter 9, Example 4</title>
-</head>
-<body>
-    
-</body>
-</html>
+let divAdvert = document.getElementById("divAdvert");
+divAdvert.style.fontStyle = "italic";
+divAdvert.style.textDecoration = "underline";
