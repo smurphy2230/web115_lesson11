d1b2ff4043c818523a6c066d9368b31fec86450c
smurphy2230 <smurphy2230@gmail.com>
2020-03-31 10:18:22 -0400

    doesn't bounce back and forth

--- ch9ex6.js
+++ ch9ex6.js
@@ -1,1 +1,25 @@
-console.log('it works');
+var switchDirection = false;
+
+function doAnimation() {
+    var divAdvert = document.getElementById("divAdvert");
+    var currentLeft = divAdvert.offsetLeft;
+    var newLocation;
+
+    if (!switchDirection) {
+        newLocation = currentLeft + 2;
+
+        if (currentLeft >= 400) {
+            switchDirection = true;
+        } else {
+            newLocation = currentLeft - 2;
+
+            if (currentLeft <= 0) {
+                switchDirection = false;
+            }
+        }
+    }
+
+    divAdvert.style.left = newLocation + "px";
+}
+
+setInterval (doAnimation, 10);
